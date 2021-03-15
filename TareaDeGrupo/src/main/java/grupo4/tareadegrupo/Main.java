a5210b8b61efa449f8bf0ee081d21df360319d91
erikzelaya60 <erikzelaya60@gmail.com>
2021-03-14 20:06:56 -0600

    utilizando clases creadas en main para imprimir informacion

--- TareaDeGrupo/src/main/java/grupo4/tareadegrupo/Main.java
+++ TareaDeGrupo/src/main/java/grupo4/tareadegrupo/Main.java
@@ -5,6 +5,9 @@
  */
 package grupo4.tareadegrupo;
 
+import grupo4.tareadegrupo.clases.Farmacia;
+import grupo4.tareadegrupo.clases.Laboratorio;
+
 /**
  *
  * @author erikzelaya60
@@ -15,6 +18,34 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
+        Farmacia farmacia = new Farmacia();
+        farmacia.setEncargadoFarmacia("Odalys Quintano");
+        farmacia.setTelefonoEncargado("99887766");
+        
+        System.out.println("Encargado de farmacia: " + farmacia.getEncargadoFarmacia());
+        System.out.println("Telefono del encargado de farmacia: " + farmacia.getTelefonoEncargado());
+        System.out.println("Ubicacion edificio: " + farmacia.getUbicacionEdificio());
+        System.out.println("Color edificio: " + farmacia.getColorEdificio());
+        System.out.println("Numero de pisos: " + farmacia.getNumeroDePisos());
+        System.out.println("Dimensiones del edificio: " + farmacia.getDimensionesEdificio());
+        
+        Farmacia farmacia2 = new Farmacia("Erik Zelaya", "99998888");
+        
+        System.out.println("(Usando constructor) Encargado de farmacia 2: " + farmacia2.getEncargadoFarmacia());
+        System.out.println("(Usando constructor) Telefono del encargado de farmacia 2: " + farmacia2.getTelefonoEncargado());        
+        
+        Laboratorio laboratorio = new Laboratorio();
+        laboratorio.setEncargadoLaboratorio("Kevin Morazan");
+        laboratorio.setTelefonoEncargado("89897766");
+        
+        System.out.println("Encargado de laboratorio: " + laboratorio.getEncargadoLaboratorio());
+        System.out.println("Telefono del encargado de lab: " + laboratorio.getTelefonoEncargado());
+        System.out.println("Ubicacion edificio: " + laboratorio.getUbicacionEdificio());
+        System.out.println("Color edificio: " + laboratorio.getColorEdificio());
+        System.out.println("Numero de pisos: " + laboratorio.getNumeroDePisos());
+        System.out.println("Dimensiones del edificio: " + laboratorio.getDimensionesEdificio());
         
     }
     
